d01e2b61b47837f0d02e0e7883166b8f0a5446a4
Alex Hadley <alexhad6@gmail.com>
2021-05-30 16:35:35 -0400

    Simplified code

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,31 @@
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(App(
+  runApp(WidgetsApp(
+    pageRouteBuilder: <T>(settings, build) => PageRouteBuilder(
+      pageBuilder: (context, _, __) => build(context),
+    ),
+    home: HomePage(),
     title: 'Line Prototype',
     textStyle: const TextStyle(
       color: Color(0xFF000000),
-      fontSize: 30.0,
+      fontSize: 36.0,
       fontWeight: FontWeight.bold,
     ),
-    home: HomePage(
-      title: 'Line Prototype',
-      backgroundColor: Color(0xFFFFFFFF),
-    ),
+    color: Color(0xFF0000FF),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
-class App extends StatelessWidget {
-  App({
-    Key? key,
-    required this.title,
-    required this.textStyle,
-    required this.home,
-  }) : super(key: key);
-
-  final String title;
-  final TextStyle textStyle;
-  final Widget home;
-
-  PageRoute<T> _pageRouteBuilder<T>(
-      RouteSettings settings, WidgetBuilder build) {
-    return PageRouteBuilder<T>(
-      pageBuilder: (context, _, __) => build(context),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return WidgetsApp(
-      pageRouteBuilder: _pageRouteBuilder,
-      textStyle: textStyle,
-      home: home,
-      title: title,
-      color: Color(0xFF0000FF),
-    );
-  }
-}
-
 class HomePage extends StatelessWidget {
-  HomePage({
-    Key? key,
-    required this.title,
-    required this.backgroundColor,
-  }) : super(key: key);
-
-  final String title;
-  final Color backgroundColor;
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: backgroundColor,
-        child: Container(
+    return Container(
+      color: Color(0xFFFFFFFF),
+      child: SafeArea(
+        child: Align(
           alignment: Alignment.topCenter,
-          color: Color(0xFF0000FF),
-          height: 0.1,
-          child: Text(title),
+          child: Text('Line Prototype'),
         ),
       ),
     );
